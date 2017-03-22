851683ebbe484131c788a53c54cbf8cb3ddb7bb0
Jinseok Kim <jinsax@gmail.com>
2017-03-22 16:08:20 +0900

    wiki file name update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 export DATA_FOLDER="data"
 
 # Data url
-DATA_URL="https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2"
+DATA_URL="https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2"
 mkdir $DATA_FOLDER
 
 # Install required python libraries
@@ -12,7 +12,7 @@ pip install -r requirements.txt
 
 # Download data
 echo "Downloading Wikipedia articles dump.."
-curl -sS $DATA_URL > "$DATA_FOLDER/enwiki-latest-pages-articles.xml.bz2"
+curl -sS $DATA_URL > "$DATA_FOLDER/enwiki-latest-pages-articles-multistream.xml.bz2"
 
 # Run python script from training the model
 python train.py
